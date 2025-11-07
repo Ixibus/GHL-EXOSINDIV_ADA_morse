430283d333c6d850aa6379475440e00cc47bb668
Franck CORCHER <corcher.franck@gmail.com>
2025-11-07 13:14:56 +0100

    etape1à3

---
+++ script.js
@@ -0,0 +1,66 @@
+// DICTIONNAIRE
+// Annexe 1
+const latinToMorse = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
+
+// Etape 1 : Dans un fichier JavaScript, commencez par écrire une fonction getLatinCharacterList. Cette fonction va prendre en paramètre du texte et retourner un tableau contenant chaque caractère.
+
+function getLatinCharacterList(string) {
+//   console.log("*** Etape 1 ***");
+//   console.log(string.split(""));
+  return string.split("");
+}
+
+getLatinCharacterList("un test de ouf");
+
+// Etape 2 : Créer une fonction translateLatinCharacter qui prend un paramètre un caractère et renvoie sa correspondance en morse.
+
+function translateLatinCharacter(letter) {
+//   console.log("\n*** Etape 2 ***");
+//   console.log(latinToMorse[letter]);
+  return latinToMorse[letter];
+}
+
+translateLatinCharacter("A");
+
+// Etape 3 : Ajouter une nouvelle fonction encode qui prend en paramètre du texte et qui va utiliser la fonction de l’étape 1, pour chaque caractère, appliquer la fonction de l’étape 2 et ainsi récupérer son équivalent morse.
+
+function encode(string) {
+  console.log("\n*** Etape 3 ***");
+  let encodeArray = [];
+  getLatinCharacterList(string).forEach((el) => {
+    encodeArray.push(translateLatinCharacter(el));
+});
+return (encodeArray.join(""));
+}
+// console.log(encode("BONJOURJEMAPPELLEFRANCK"));
+
+
+// Etape 4 : appliquez les procédés similaire à ce qui a été fait pour le encode pour créer une fonction decode. Dans cet exercice, on admettra que les lettres en morse sont séparées par un espace, et les mots par des “/” (slash).
+// Ainsi, créer la fonction getMorseCharacterList ainsi que translateMorseCharacter.
