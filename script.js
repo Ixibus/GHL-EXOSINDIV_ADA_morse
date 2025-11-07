73cb0d54eab1c08adb17ca6f9641ab2b3f083d61
Franck CORCHER <corcher.franck@gmail.com>
2025-11-07 15:59:58 +0100

    etape_4_finiiiii

--- script.js
+++ script.js
@@ -29,11 +29,40 @@ const latinToMorse = {
   Z: "--..",
 };
 
+const morseToLatin = {
+  "-": "T",
+  "--": "M",
+  "---": "O",
+  "--.": "G",
+  "--.-": "Q",
+  "--..": "Z",
+  "-.": "N",
+  "-.-": "K",
+  "-.--": "Y",
+  "-.-.": "C",
+  "-..": "D",
+  "-..-": "X",
+  "-...": "B",
+  ".": "E",
+  ".-": "A",
+  ".--": "W",
+  ".---": "J",
+  ".--.": "P",
+  ".-.": "R",
+  ".-..": "L",
+  "..": "I",
+  "..-": "U",
+  "..-.": "F",
+  "...": "S",
+  "...-": "V",
+  "....": "H",
+};
+
 // Etape 1 : Dans un fichier JavaScript, commencez par écrire une fonction getLatinCharacterList. Cette fonction va prendre en paramètre du texte et retourner un tableau contenant chaque caractère.
 
 function getLatinCharacterList(string) {
-//   console.log("*** Etape 1 ***");
-//   console.log(string.split(""));
+  //   console.log("*** Etape 1 ***");
+  //   console.log(string.split(""));
   return string.split("");
 }
 
@@ -42,8 +71,8 @@ getLatinCharacterList("un test de ouf");
 // Etape 2 : Créer une fonction translateLatinCharacter qui prend un paramètre un caractère et renvoie sa correspondance en morse.
 
 function translateLatinCharacter(letter) {
-//   console.log("\n*** Etape 2 ***");
-//   console.log(latinToMorse[letter]);
+  //   console.log("\n*** Etape 2 ***");
+  //   console.log(latinToMorse[letter]);
   return latinToMorse[letter];
 }
 
@@ -56,11 +85,37 @@ function encode(string) {
   let encodeArray = [];
   getLatinCharacterList(string).forEach((el) => {
     encodeArray.push(translateLatinCharacter(el));
-});
-return (encodeArray.join(""));
+  });
+  return encodeArray.join("");
 }
 // console.log(encode("BONJOURJEMAPPELLEFRANCK"));
 
-
 // Etape 4 : appliquez les procédés similaire à ce qui a été fait pour le encode pour créer une fonction decode. Dans cet exercice, on admettra que les lettres en morse sont séparées par un espace, et les mots par des “/” (slash).
 // Ainsi, créer la fonction getMorseCharacterList ainsi que translateMorseCharacter.
+
+function getMorseCharacterList(string) {
+  return string.split("/");
+}
+// getMorseCharacterList("..- -./- . ... -/-.. ./ --- ..- ..-.");
+
+function translateMorseCharacter(char) {
+  return morseToLatin[char];
+}
+// console.log(translateMorseCharacter("---"));
+
+function decode(morseStr) {
+  let decodeArray = [];
+  // getMorseCharacterList(morseStr).forEach(el => {
+  //   decodeArray.push(translateMorseCharacter(el));
+  // });
+  // return decodeArray;
+  getMorseCharacterList(morseStr).forEach((morseWord) => {
+    morseWord.split("/").forEach((morseLetter) => {
+      // morseLetter === " " ? decodeArray.push("") : decodeArray.push(translateMorseCharacter(morseLetter));
+      decodeArray.push((morseLetter.split(" ").map(el => (translateMorseCharacter(el))).join(""))) ;
+    });
+  });
+  return(decodeArray.join(" "));
+}
+
+console.log(decode("..- -./- . ... -/-.. ./--- ..- ..-."));
